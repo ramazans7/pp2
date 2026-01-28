2751ea582a945dc48c83e63223612e367cac0772
Ramazan <serikbay.ramazann7@gmail.com>
2026-01-28 08:07:36 +0500

    Add Practice1 Python basics examples

--- week1/syntax.py
+++ week1/syntax.py
@@ -1,3 +1,19 @@
 # Example 1:
 x = 25
 a = "Hello world!"
+#Example 2:
+if 5 > 2:
+    print("Five is greater than two!") 
+#Example 3:
+x = 5
+y = "Hello, World!"
+
+#Example 4:
+
+a=4
+print(a*4)
+
+#Example 5:
+a=5
+b=8
+print(a+b)
