5e08b96af509151950fd92aa63214246562d062b
Ramazan <serikbay.ramazann7@gmail.com>
2026-01-28 08:18:44 +0500

    Add Practice1 Python basics examples

---
+++ week1/variables.py
@@ -0,0 +1,28 @@
+#Example 1
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#Example 2
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#Example 3
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+#Example 4
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#Example 5
+
+a = 4
+A = "Sally"
+#A will not overwrite a
