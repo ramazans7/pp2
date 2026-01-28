c058470a2d6cecc85181da011455c4759d108953
Ramazan <serikbay.ramazann7@gmail.com>
2026-01-28 08:28:50 +0500

    Add Practice1 Python basics examples

---
+++ week1/strings.py
@@ -0,0 +1,21 @@
+#example 1
+print("Hello")
+
+#example 2
+print('Hello')
+
+#example 3
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+#example 4
+a = "Hello"
+print(a)
+
+#example 5
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
