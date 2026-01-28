fe8d0702d38b8aea861326da52d00b61216f748c
Ramazan <serikbay.ramazann7@gmail.com>
2026-01-28 08:21:29 +0500

    Add Practice1 Python basics examples

---
+++ week1/datatypes.py
@@ -0,0 +1,20 @@
+#Example 1
+x = 5
+print(type(x))
+
+#Example 2
+x = "Hello World"
+print(type(x))
+
+#Example 3
+
+x = 20.5
+print(type(x))
+
+#Example 4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+#Example 5
+x = True
+print(type(x))
